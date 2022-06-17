8ac6f309e6f6ad33f425c39b3650883dd7d1a158
Roman Krivenko <romakrivenko@gmail.com>
2022-06-18 00:27:10 +0300

    fixed history

--- src/js/app.js
+++ src/js/app.js
@@ -12,3 +12,22 @@ const scrollBtn = document.getElementById('topBtn');
 scrollBtn.addEventListener('click', () => scrollTop(0));
 
 window.onscroll = function() {scrollFunction()};
+
+const sections = document.querySelectorAll('.history__item');
+const navigation = document.querySelectorAll('.history__year');
+const toogleActiveItem = () => {
+    let activeSection = '';
+    sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        if (scrollY >= sectionTop ) {
+            activeSection = section.getAttribute('id'); }
+    });
+    navigation.forEach((navItem) => {
+        navItem.classList.remove('current_year');
+        if (navItem.classList.contains(activeSection)) {
+            navItem.classList.add('current_year');
+        }
+    });
+}
+
+window.addEventListener('scroll', toogleActiveItem);
